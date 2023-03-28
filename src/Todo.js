4b4a8d4161c38d0a07352f06827077ec1c8031db
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-28 01:38:43 -0700

    building ui for todo list in Todo.js

---
+++ src/Todo.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { addTodo, clearTodos, removeTodo } from "./features/todoSlice"
+
+function Todo() {
+    const items = useSelector((state) => state.todo.items)
+
+    const dispatch = useDispatch()
+
+    const [input, setInput] = useState('')
+
+    const todoSubmit = (e) => {
+        e.preventDefault()
+        dispatch(addTodo(input))
+    }
+
+    const todoItems = items.map((item, index) =>
+        <li key={index} style={{paddingBottom: "15px"}}>
+            {item}<span style={{marginLeft: "10px"}}><button onClick={() => dispatch(removeTodo())}>x</button></span>
+        </li>
+    )
+
+    return (
+        <div>
+            <h1>
+                Todo List
+            </h1>
+            <form onSubmit={(e) => todoSubmit(e)}>
+                <input type="text" onChange={(e) => setInput(e.target.value)} />
+                <button type="submit">Add Todo</button>
+            </form>
+            <button onClick={() => dispatch(clearTodos())}>
+                Clear Todos
+            </button>
+            <ul style={{listStyleType: "none"}}>
+                {todoItems}
+            </ul>
+        </div>
+    )
+}
+
+export default Todo
